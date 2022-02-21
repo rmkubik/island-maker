6b475cb0590796e34cc0b7b6cde6b1d5673dd496
Ryan Kubik <rmkubik@me.com>
2022-02-21 10:18:45 -0800

    Tweak visual offset pointer calculation

--- src/utils/getHexFromPointerEvent.js
+++ src/utils/getHexFromPointerEvent.js
@@ -3,7 +3,7 @@ import { TILE_HEIGHT, VISUAL_Y_OFFSET } from "../data/config";
 const getHexFromPointerEvent = (GridData, scale) => (event) => {
   const hex = GridData.pointToHex(
     event.clientX / scale,
-    (event.clientY - VISUAL_Y_OFFSET) / scale - TILE_HEIGHT
+    event.clientY / scale - TILE_HEIGHT - VISUAL_Y_OFFSET
   );
 
   return hex;
